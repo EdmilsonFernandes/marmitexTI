1938e6a0c9752db538896d99da1f70e255e2a9d3
esantos <esaantos@fitec.org.br>
2018-01-25 15:10:43 -0200

    Initial site

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "TI Delivery Marmitex ED software";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Entre contato com conosco";
+
+            return View();
+        }
+    }
+}
